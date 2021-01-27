240958f03791d5226233a66f39aae405f1c6f791
mewiemers <m.wiemers01@gmail.com>
2021-01-27 14:22:10 +0100

    add container

--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -2,6 +2,7 @@ import "./card.css";
 import cardOffers from "./card-offers.html";
 import cardFruits from "./card-fruits.html";
 import cardDrinks from "./card-drinks.html";
+import cardContainer from "./card.container.html";
 
 export default {
   title: "Components/Card",
@@ -11,3 +12,4 @@ export default {
 export const offer = () => cardOffers;
 export const fruits = () => cardFruits;
 export const drinks = () => cardDrinks;
+export const Container = () => cardContainer;
