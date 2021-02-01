bf61e22e505c7fb995c874e7d7a3b57dd8118e6e
mewiemers <m.wiemers01@gmail.com>
2021-02-01 17:46:24 +0100

    fix forms

--- src/components/form/register-forms.js
+++ src/components/form/register-forms.js
@@ -1,10 +1,36 @@
+// export function createRegisterForm() {
+//   return `
+//     <form class="form">
+//     <h2>Reset Password</h2>
+//     <p>Please enter your email to receive a link to create a new password via email</p>
+//     <input placeholder="email"/>
+//     <button>Register</button>
+//     </form>
+//     `;
+// }
+
 export function createRegisterForm() {
-  return `
-    <form class="form">
-    <h2>Reset Password</h2>
-    <p>Please enter your email to receive a link to create a new password via email</p>
-    <input placeholder="email"/>
-    <input type="submit"/>
-    </form>
-    `;
+  const form = document.createElement("form");
+  form.className = "form";
+
+  const button = document.createElement("button");
+  button.innerText = "Reset";
+  button.className = "btn-text";
+
+  function register() {
+    alert("Registered");
+  }
+  button.addEventListener("click", register);
+
+  const title = document.createElement("h2");
+  title.innerText = "Reset password";
+  title.className = "title-text";
+
+  const input = document.createElement("input");
+  input.placeholder = "Trag was ein";
+  input.className = "input";
+
+  form.append(title, input, button);
+
+  return form;
 }
