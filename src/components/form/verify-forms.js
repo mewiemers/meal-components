a29a251f3ba9da7d530113f8962fdca42d4db9e5
mewiemers <m.wiemers01@gmail.com>
2021-02-03 13:46:34 +0100

    :wrench: update container

--- src/components/form/verify-forms.js
+++ src/components/form/verify-forms.js
@@ -29,6 +29,17 @@ function createinfotext() {
 
   return infotext;
 }
+function createPasswordContainerElement() {
+  const passwordContainer = document.createElement("div");
+  passwordContainer.className = "form__otp";
+  const field1 = createOTPInput();
+  const field2 = createOTPInput();
+  const field3 = createOTPInput();
+  const field4 = createOTPInput();
+  passwordContainer.append(field1, field2, field3, field4);
+
+  return passwordContainer;
+}
 
 export function createVerifyForms() {
   const verify = document.createElement("form");
@@ -43,14 +54,7 @@ export function createVerifyForms() {
   const title = createHeadLine();
   const infotext = createinfotext();
 
-  const field1 = createOTPInput();
-  const field2 = createOTPInput();
-  const field3 = createOTPInput();
-  const field4 = createOTPInput();
-
-  const passwordContainer = document.createElement("div");
-  passwordContainer.className = "form__otp";
-  passwordContainer.append(field1, field2, field3, field4);
+  const passwordContainer = createPasswordContainerElement();
 
   const hint = document.createElement("p");
   hint.innerText = "Didn`t Receive?";
